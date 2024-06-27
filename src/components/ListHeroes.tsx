03f07c0d94376af8d26650fd2cc0a6b7d931d4f9
jhosuapp <jhosuapvll@gmail.com>
2024-06-27 08:00:29 -0500

    fix: error data && list heroes

--- src/components/ListHeroes.tsx
+++ src/components/ListHeroes.tsx
@@ -7,23 +7,18 @@ type Props = {
 const ListHeroes = ( { publisher }: Props )=>{
     const dataHeroes = useHeroes(state => state.dataHeroes);
     const validPublisher = ['Marvel Comics', 'DC Comics'];
-
+    //Validate if publisher is valid
     if(!validPublisher.includes( publisher )){
         throw new Error(`Publisher ${ publisher } no es correcto`);
     }
-    const duplicateHeroes = [...dataHeroes];
+    //Filter by publisher
+    const filterByPublisher = dataHeroes.filter(item => item.publisher === publisher);
 
     return(
         <ul>
-            <li>hola</li>
-            <li>hola</li>
-            <li>hola</li>
-            <li>hola</li>
-            <li>hola</li>
-            <li>hola</li>
-            <li>hola</li>
-            <li>hola</li>
-            <li>hola</li>
+            {filterByPublisher.length && filterByPublisher.map(({ superhero }, index)=>(
+                <li key={ index }>{ superhero }</li>
+            ))}
         </ul>
     );
 }
--- src/screens/Marvel/MarvelScreen.tsx
+++ src/screens/Marvel/MarvelScreen.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from 'react';
+import { useHeroes } from '../../store/';
+import { ListHeroes, Container } from '../../components';
+
 const MarvelScreen = ():JSX.Element =>{
+    const getDataHeroes = useHeroes(state => state.getDataHeroes);
+    const statusHeroes = useHeroes(state => state.statusHeroes);
+
+    useEffect(()=>{
+        getDataHeroes();
+    },[]);
+
     return( 
-        <h1>MarvelScreen dos</h1>
+        <Container className='ctn ctn--top ctn--fullheight'>
+            <article>
+                <h1>MARVEL SCREEN</h1>
+                {statusHeroes == 'success' && <ListHeroes publisher='Marvel Comics' />}
+            </article>
+        </Container>
     );
 }
 
