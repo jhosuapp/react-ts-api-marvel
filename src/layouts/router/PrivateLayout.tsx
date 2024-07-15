b25d8a897f558ea20b76cb80c097fda4e5abd825
jhosuapp <jhosuapvll@gmail.com>
2024-07-15 10:17:52 -0500

    feat: protec routes

--- src/layouts/router/PrivateLayout.tsx
+++ src/layouts/router/PrivateLayout.tsx
@@ -1,8 +1,18 @@
 
+import { useAuth } from '../../store/index';
 import { Navbar } from '../../layouts/header/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const PrivateLayout = ():JSX.Element =>{
+
+    const user = useAuth(state => state.user);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        user.logged === false && navigate('/');
+    }, [user]);
+
     return( 
         <>
             <Navbar enableNav />
--- src/layouts/router/PublicLayout.tsx
+++ src/layouts/router/PublicLayout.tsx
@@ -1,8 +1,18 @@
 
-import { Outlet } from 'react-router-dom';
-import { Navbar } from '../header/Navbar';
+import { useAuth } from '../../store/index';
+import { Navbar } from '../../layouts/header/Navbar';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const PublicLayout = ():JSX.Element =>{
+
+    const user = useAuth(state => state.user);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        user.logged === true && navigate('/marvel');
+    }, [user]);
+
     return( 
         <>
             <Navbar />
